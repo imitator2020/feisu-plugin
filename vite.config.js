79085b361a731f7abc7998c311fa2fb6761a1984
chenyeru <chenyeru@corp.netease.com>
2025-07-12 23:32:10 +0800

    feat: github pages设置

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
+    base: "feisu-plugin",
     plugins: [react()],
     server: {
         host: "0.0.0.0",
